a81fb40fe54460388595a61992c97742c2e9f818
Aurélien <reyaurelien.business@gmail.com>
2022-02-03 22:45:10 +0100

    add fixtures wishlist

--- src/DataFixtures/Wishlist.php
+++ src/DataFixtures/Wishlist.php
@@ -2,48 +2,44 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\BdColec;
+use App\Entity\wishlistColec;
 use App\Entity\Genres;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class Bd extends Fixture
+class Wishlist extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $bds = [
+        $wishlists = [
             ['title' => 'Blacksad',
-                'edition' => 'Dargaux',
                 'Tome' => '1',
-                'image' => 'https://bdi.dlpdomain.com/album/9782205049657-couv.jpg',
+                'image' => 'https://wishlisti.dlpdomain.com/album/9782205049657-couv.jpg',
                 'genres' => 'Aventure'],
             ['title' => 'Blacksad : artic-nation',
-                'edition' => 'Dargaux',
                 'Tome' => '2',
-                'image' => 'https://bdi.dlpdomain.com/album/9782205051995-couv.jpg',
+                'image' => 'https://wishlisti.dlpdomain.com/album/9782205051995-couv.jpg',
                 'genres' => 'Aventure'],
             ['title' => 'OKKO : cycle du vent',
-                'edition' => 'Delcourt',
                 'Tome' => '1',
                 'image' => 'https://www.bedetheque.com/media/Couvertures/Couv_41693.jpg',
                 'genres' => 'Aventure'],
             ['title' => 'OKKO : cycle du vent',
-                'edition' => 'Delcourt',
                 'Tome' => '2',
                 'image' => 'https://static.fnac-static.com/multimedia/Images/FR/NR/ce/13/1b/1774542/1507-1/tsp20200429070933/Okko-T02-Le-cycle-de-l-eau-2.jpg',
                 'genres' => 'Aventure']];
 
 
-        foreach ($bds as $bd) {
-            $bdT = new BdColec();
+        foreach ($wishlists as $wishlist) {
+            $wish = new \App\Entity\Wishlist();
             $genre = new Genres();
-            $bdT->setTitle($bd['title']);
-            $bdT->setEdition($bd['edition']);
-            $bdT->setTome($bd['Tome']);
-            $bdT->setImage($bd['image']);
-            $manager->persist($bdT);
+            $wish->setTitle($wishlist['title']);
+            $wish->setTome($wishlist['Tome']);
+            $wish->setImage($wishlist['image']);
+            $manager->persist($wish);
         }
         $manager->flush();
 
     }
+
 }
